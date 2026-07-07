d9d046e477515827336c9cdc51e633626c5d7901
Brunno Xavier De Oliveira <Brunno.mkti@gmail.com>
2026-07-07 21:30:23 +0100

    fix: liberar cors para frontend vercel

--- mesalivre-api/src/main/java/com/brunno/mesalivre/config/CorsConfig.java
+++ mesalivre-api/src/main/java/com/brunno/mesalivre/config/CorsConfig.java
@@ -15,7 +15,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://mesa-livre-nine.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
